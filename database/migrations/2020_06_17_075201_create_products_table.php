3a31d63563d284cd1830ba09afb05c8365ee299d
Zuhad Shoesmart <zuhadachmadfachrian@gmail.com>
2020-06-17 15:32:06 +0700

    make migration categori, product, news, banner

---
+++ database/migrations/2020_06_17_075201_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_code');
+            $table->string('product_name');
+            $table->bigInteger('product_price');
+            $table->text('product_description');
+            $table->date('product_store'); //produk masuk
+            $table->date('product_exp'); //kadaluarsa
+            $table->unsignedBigInteger('categories_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
